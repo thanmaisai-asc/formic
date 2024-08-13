276c419296768555d58481062a1e75b8985c6176
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-13 15:22:17 +0530

    Implement basic form with email, age, password, and confirm password fields

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,19 +1,64 @@
-import { Formik, useFormik } from "formik";
+import { useFormik } from "formik";
+
 const BasicForm = () => {
   const formik = useFormik({
-    initialValues:{
-      email:"",
-    }
-  })
-  console.log(formik)
-    return (
-      <form autoComplete="off">
-        <label htmlFor="email">Email</label>
-        <input
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      confirmPasswords: "",
+    },
+  });
+
+  console.log(formik);
+
+  return (
+    <form autoComplete="off">
+      <label htmlFor="email">Email</label>
+      <input
         value={formik.values.email}
         onChange={formik.handleChange}
-        id="email" type="email" placeholder="Enter your email" />
-      </form>
-    );
-  };
-  export default BasicForm;
+        onBlur={formik.handleBlur}
+        id="email"
+        name="email"
+        type="email"
+        placeholder="Enter your email"
+      />
+
+      <label htmlFor="age">Age</label>
+      <input
+        id="age"
+        name="age"
+        type="number"
+        placeholder="Enter your age"
+        value={formik.values.age}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        id="password"
+        name="password"
+        type="password"
+        placeholder="Enter your password"
+      />
+
+      <label htmlFor="confirmPasswords">Confirm Passwords</label>
+      <input
+        id="confirmPasswords"
+        name="confirmPasswords"
+        type="password"
+        placeholder="Confirm Password"
+        onChange={formik.handleChange}
+        value={formik.values.confirmPasswords}
+        onBlur={formik.handleBlur}
+      />
+    </form>
+  );
+};
+
+export default BasicForm;
